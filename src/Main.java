9fbe5f32f98ba23e3885427a244ac86d0682472b
AlexV-Ch <alexch@yandex.ru>
2025-01-22 21:53:10 +0300

    8.10 Практическая работа (автотесты) задание 1

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        TrucksAndContainers pr1 = new TrucksAndContainers(54);
+        pr1.getKolvoKonAvto();
+    }
+}
---
+++ src/TrucksAndContainers.java
@@ -0,0 +1,82 @@
+public class TrucksAndContainers {
+    private int yashchKont = 27;
+    private int kontAuto = 12;
+    private int kolvoYashchikov;
+
+    public TrucksAndContainers(int kolvoYashchikov) {
+        if (kolvoYashchikov <= 0) {
+            this.kolvoYashchikov = 0;
+        } else {
+            this.kolvoYashchikov = kolvoYashchikov;
+        }
+    }
+
+    public int getKolvoYashchikov() {
+        return kolvoYashchikov;
+    }
+
+    public void setKolvoYashchikov(int kolvoYashchikov) {
+        this.kolvoYashchikov = kolvoYashchikov;
+    }
+
+    public void getKolvoKonAvto() {
+        System.out.println("Количество ящиков - " + getKolvoYashchikov());
+        System.out.println("Количество контейнеров - " + getKolvoKon());
+        System.out.println("Количество автомобилей - " + getKolvoAuto());
+        if (kolvoYashchikov == 0) {
+            System.out.println("Где груз?");
+        } else {
+            for (int i = 1; i <= kolvoYashchikov; i++) {
+                System.out.println("Ящик - " + i);
+            }
+        }
+
+    }
+
+
+
+// Расчет кол-ва контейнеров
+    public int getKolvoKon() {
+        if (kolvoYashchikov == 0) {
+            return 0;
+        } else {
+            int kolvoKon = kolvoYashchikov / yashchKont;
+            int kolvoYaOst = kolvoYashchikov % yashchKont;
+            if (kolvoKon == 0) {
+                return 1;
+            } else {
+                if (kolvoKon > 0 && kolvoYaOst == 0) {
+                    return kolvoKon;
+                } else {
+                    return kolvoKon + 1;
+                }
+            }
+        }
+    }
+
+    // Расчет кол-ва машин
+    public int getKolvoAuto() {
+        if (kolvoYashchikov == 0) {
+            return 0;
+        } else {
+            int kolvoAvt = getKolvoKon() / kontAuto;
+            int kolvoKonOst = getKolvoKon() % kontAuto;
+            if (kolvoAvt == 0) {
+                return 1;
+            } else {
+                if (kolvoAvt > 0 && kolvoKonOst == 0) {
+                    return kolvoAvt;
+                } else {
+                    return kolvoAvt + 1;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+    }
+
